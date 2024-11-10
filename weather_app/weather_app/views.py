b01a6d8532063d65fa8809e136a2c469323ba359
Nikhil Sharma <nikhil23062000@icloud.com>
2024-11-09 19:38:12 -0500

    Revert "fixed some bugs"
    This reverts commit 05d9e87abfa232cc0965704143a11e76e0d9afcf.

--- weather_app/weather_app/views.py
+++ weather_app/weather_app/views.py
@@ -21,7 +21,6 @@ def home(request):
             return JsonResponse(response.json())
     except Exception as e:
         print(f'{e}')
-        return JsonResponse(e)
 
 def main(request):
     return HttpResponse("Your server is working")
